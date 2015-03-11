03e2828241e3b1dfa7395966824019c358a604f2
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-03-11 14:39:47 -0400

    optimization

--- email_scraper.py
+++ email_scraper.py
@@ -3,10 +3,6 @@ file = open("toronto_bus_list.txt", "r")
 out = open("email_list.txt", "w")
 file_str = file.read()
 pattern = re.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-for email in re.findall(pattern, file_str):
-	print(email) #debugging purposes
-	out.write(email + ", \n")
-
-
+out.write(', \n'.join(re.findall(pattern, file_str)))
 out.close()
 file.close()
