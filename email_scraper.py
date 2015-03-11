dc015e557c60126ce25274ec3ff7150093686e1d
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-03-11 14:29:59 -0400

    oops

--- email_scraper.py
+++ email_scraper.py
@@ -1,6 +1,6 @@
 import re
 file = open("toronto_bus_list.txt", "r")
-out = open("email_list", "w")
+out = open("email_list.txt", "w")
 file_str = file.read()
 pattern = re.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
 for email in re.findall(pattern, file_str):
